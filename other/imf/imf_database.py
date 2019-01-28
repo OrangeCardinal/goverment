5cf5913f270bd08e0478af7b0ed62f7e4e72dbfe
OrangeCardinal <david.scott.orkin@gmail.com>
2019-01-28 07:38:01 -0700

    Collection of all database ids in the IMF data set.

---
+++ other/imf/imf_database.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+class IMF_Database(Enum):
+    ASIA_AND_PACIFIC_REGIONAL_ECONOMIC_OUTLOOK          = "APDREO"
+    BALANCE_OF_PAYMENTS                                 = "BOP"
+    COORDINATED_DIRECT_INVESTMENT_SURVEY                = "CDIS"
+    COORDINATED_PORTFOLIO_INVESTMENT_SURVEY             = "CPIS"
+    CONSUMER_PRICE_INDEX                                = "CPI"
+    COVERAGE_OF_FISCAL_REPORTING                        = "COFR"
+    CURRENCY_COMPOSITION_OF_FOREIGN_EXCHANGE_RESERVES   = "COFER"
+    DIRECTION_OF_TRADE_STATISTICS                       = "DOT"
+    FINANCIAL_ACCESS_SURVEY                             = "FAS"
+    FINANCIAL_SOUNDNESS_INDICATORS                      = "FSI"
+    FISCAL_MONITOR                                      = "FM"
+    HISTORICAL_PUBLIC_DEBT                              = "HPDD"
+    PRINCIPLE_GLOBAL_INDICATORS                         = "PGI"
+    WORLD_ECONOMIC_OUTLOOK                              = "WEO"
